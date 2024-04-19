5ccfdaa385d1a39d1389fb32b237e5e07d4a2543
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-19 15:13:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,5 +6,4 @@ from library.buttons import reply_markup_start
 @Client.on_message(filters.private & filters.command(['start', 'help']))
 async def start_bot(bot, m: Message):
     await m.reply_text(Presets.WELCOME_MSG.format(m.from_user.first_name),
-                       reply_markup=reply_markup_start,
-                       parse_mode='markdown')
+                       reply_markup=reply_markup_start)
