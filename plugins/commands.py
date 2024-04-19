8a4f8019a6bf9fe8fe0d78dcb504f807885f4381
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-19 15:20:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,7 +1,6 @@
-from presets import Presets
 from pyrogram.types import Message
 from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command(['start', 'help']))
 async def start_bot(bot, m: Message):
-    await m.reply_text(Presets.WELCOME_MSG.format(m.from_user.first_name))
+    await m.reply_text("You have started this bot.")
