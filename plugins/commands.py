5ed4fae72fc8495d6ba481ae8e8e4ba304b19b1c
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-19 15:15:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,9 +1,7 @@
 from presets import Presets
 from pyrogram.types import Message
 from pyrogram import Client, filters
-from library.buttons import reply_markup_start
 
 @Client.on_message(filters.private & filters.command(['start', 'help']))
 async def start_bot(bot, m: Message):
-    await m.reply_text(Presets.WELCOME_MSG.format(m.from_user.first_name),
-                       reply_markup=reply_markup_start)
+    await m.reply_text(Presets.WELCOME_MSG.format(m.from_user.first_name))
