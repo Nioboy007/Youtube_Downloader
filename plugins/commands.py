27589cde8a0c27fd599f1aebbab768b4a1204c99
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-19 14:59:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,8 +1,3 @@
-# Name     : Youtube_Downloader 'Inline' [ Telegram ]
-# Repo     : https://github.com/Rexinazor/Youtube_Downloader
-# Author   : Rexinazor
-
-
 from presets import Presets
 from pyrogram.types import Message
 from pyrogram import Client, filters
@@ -11,4 +6,5 @@ from library.buttons import reply_markup_start
 @Client.on_message(filters.private & filters.command(['start', 'help']))
 async def start_bot(bot, m: Message):
     await m.reply_text(Presets.WELCOME_MSG.format(m.from_user.first_name),
-                       reply_markup=reply_markup_start)
+                       reply_markup=reply_markup_start,
+                       parse_mode='markdown')  # Add parse_mode argument here
